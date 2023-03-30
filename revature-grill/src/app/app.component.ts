036c1366a1ab9a2c08b6628ebb0edae58f266323
Nick Hodges <nickhodges@Nicks-MBP.home>
2023-03-30 15:07:30 -0400

    animations

--- revature-grill/src/app/app.component.ts
+++ revature-grill/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import AOS from 'aos';
 
 
 @Component({
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Revature-Grill';
+
+  ngOnInit() {
+    AOS.init();
+  }
 }
 
